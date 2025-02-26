97c2a5de137b09d4e5a1f3e82b8943607c7c21a0
Neasher <neasher75@gmail.com>
2025-02-27 04:53:42 +0600

    cors issue

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://quickpay-cash.netlify.app/"],
+    origin: "https://quickpay-cash.netlify.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+app.options("*", cors()); 
 
 // Database Connection
 const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gxl79.mongodb.net/quickpay?retryWrites=true&w=majority&appName=Cluster0`;
@@ -33,7 +34,7 @@ mongoose
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://quickpay-cash.netlify.app/"],
+    origin: "https://quickpay-cash.netlify.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
