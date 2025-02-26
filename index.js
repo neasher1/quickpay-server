6333097c718b7a284f7bcc53d93ff6aaafcb7418
Neasher <neasher75@gmail.com>
2025-02-27 03:37:06 +0600

    vercel json file added

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      // "https://endearing-fenglisu-dad95b.netlify.app",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 // Database Connection
 const MONGO_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gxl79.mongodb.net/quickpay?retryWrites=true&w=majority&appName=Cluster0`;
